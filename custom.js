3d986ea2a3403c65cbdfa02964745cd694520529
ChadwellL <leochadwell@gmail.com>
2017-01-17 14:24:15 +0000

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    setTimeout(function(){
+        $('.heading').fadeIn(100);
+    }, 1000);
+});
+
+$(document).ready(function () {
+    setTimeout(function(){
+        $('.content').fadeIn(100);
+    }, 2000);
+});
+
+$(document).ready(function () {
+    setTimeout(function(){
+        $('.tagline').fadeIn(100);
+    }, 3000);
+});
+
+$(document).ready(function () {
+    setTimeout(function(){
+        $('.logo').fadeIn(100);
+    }, 4000);
+});
+
+$(document).ready(function () {
+    setTimeout(function(){
+        $('.cta').fadeIn(100);
+    }, 5000);
+});
